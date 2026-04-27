123126c3cb4308969d0cb8d841647d5f8f80b76e
NikolaiWac <nikolai.waclawik@smail.inf.h-brs.de>
2026-04-27 19:52:18 +0200

    Aufgabe 1.1 a)

--- Uebung_1/src/uebung1/businesslogic/GermanFormatNumberTransformer.java
+++ Uebung_1/src/uebung1/businesslogic/GermanFormatNumberTransformer.java
@@ -1,14 +1,22 @@
 package uebung1.businesslogic;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class GermanFormatNumberTransformer implements NumberTransformer{
 
     @Override
     public String transformNumber(int number) {
-        return "";
+        if (number > 3000){
+            return "Zahl darf nicht größer als 3.000 sein.";
+        }
+        DecimalFormat germanFormat = new DecimalFormat("#.###", DecimalFormatSymbols.getInstance(Locale.GERMANY));
+        return germanFormat.format(number);
     }
 
     @Override
     public String getTransformerType() {
-        return "";
+        return "Transformer für deutsche Zahlenformatierungen";
     }
 }
--- Uebung_1/src/uebung1/businesslogic/RomanNumberTransformer.java
+++ Uebung_1/src/uebung1/businesslogic/RomanNumberTransformer.java
@@ -4,11 +4,26 @@ public class RomanNumberTransformer implements NumberTransformer{
 
     @Override
     public String transformNumber(int number) {
-        return "";
+        if (number > 3000){
+            return "Zahl darf nicht größer als 3.000 sein.";
+        }
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                result.append(numerals[i]);
+                number -= values[i];
+            }
+        }
+
+        return result.toString();
     }
 
     @Override
     public String getTransformerType() {
-        return "";
+        return "Transformer für römische Zahlen";
     }
 }
